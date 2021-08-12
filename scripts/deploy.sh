0fe4331475084c8308ffa1656c483a0205b7c0e5
okputto <okputto@naver.com>
2021-08-12 22:56:39 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,6 +33,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup jar -jar \
-        -Dspring.config.location=classpath:/application.properties, classpath:/application-real.properties, /home/ec2-user/app/application-oauth.properties, /home/ec2-user/app/application-real-db.properties \
+        -Dspring.config.location=classpath:/application.properties, classpath:/application-real.properties, /home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
